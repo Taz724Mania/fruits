50927dd77b3e952a657ceeab61081115ca798314
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-01-02 19:15:13 -0500

    everything is connected

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const morgan = require("morgan")
 const methodOverride = require("method-override")
 const mongoose = require("mongoose")
 
+//Connect Database
+const DATABASE_URL = process.env.DATABASE_URL
 
+//Establish Connection
+mongoose.connect(DATABASE_URL) //this is what actually connects to the url
 
-
-
-
-
-
-
+//Connection Events //this is what tells us if we are connected, disconnected, or if there's an error
+mongoose.connection
+.on("open", () => {console.log("connected to Mongo")})
+.on("close", () => {console.log("Disconnected from Mongo")})
+.on("error", (error) => {console.log(error)})
 
 
 
